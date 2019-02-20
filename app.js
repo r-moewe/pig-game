72f03818875159e01e79963a4fe72790d30d74d5
Noriko Yoshizawa <yoshizawa@icue.jp>
2019-02-20 13:26:50 +0900

    roundScore実装

--- app.js
+++ app.js
@@ -8,3 +8,65 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+saikoro = Math.floor(Math.random() * 6);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').innerHTML = 0;
+document.getElementById('current-0').innerHTML = 0;
+document.getElementById('score-1').innerHTML = 0;
+document.getElementById('current-1').innerHTML = 0;
+
+
+//Roll Dice
+const btnRoll = document.querySelector('.btn-roll');
+
+//無名関数（再利用できない
+btnRoll.addEventListener('click', function () {
+
+    //random number
+    var saikoro = Math.floor(Math.random() * 6) + 1;
+
+    //display dice
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = "dice-" + saikoro + ".png";
+
+    //１じゃなければ現在の点数アップデート
+    if (saikoro !== 1) {
+        roundScore += saikoro;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player（１が出た時の処理）
+
+        //Round Scoreをリセット
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+
+        //三項演算子（もしactivePlayer===0なら？should be 1 /else:should be 0
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        //下記の式と同じ
+        // if (activePlayer === 0) {
+        //     activePlayer = 1;
+        // } else {
+        //     activePlayer = 0;
+        // }
+
+        //Active Player Change
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+
+    }
+
+});
